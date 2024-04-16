e16ba88c76b04a9be719360f4a4a81f45ebe1879
Joel Moyal <j.moyal@student.fontys.nl>
2024-04-16 16:18:57 -0600

    Adding the Feature to add a Full Name

--- main.c
+++ main.c
@@ -7,18 +7,20 @@ int main()
 
 char color[20];
 char plural_non[20];
-char Celebrity[20];
+char CelebrityF[20];
+char CelebrityL[20];
+
 
 printf ("Enter a Color: " ) ;
 scanf ("%s", color);
 printf ("Enter a plural noun: " ) ;
 scanf ("%s", plural_non);
 printf ("Enter a Celebrity: " ) ;
-scanf ("%s", Celebrity);
+scanf ("%s %s", CelebrityF, CelebrityL);
 
 printf ("Rose are %s \n", color );
 printf ("%s are blue \n", plural_non);
-printf ("I love %s\n", plural_non);
+printf ("I love %s %s\n", CelebrityF, CelebrityL) ;
     
 
     return 0 ;
